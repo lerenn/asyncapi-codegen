a1f297657b24758e1fb5e737107326e555f364d5
Louis FRADIN <louis.fradin@gmail.com>
2023-02-20 12:38:21 +0100

    fix JSON marshaling on time.Time subtypes

--- test/official/expected/generate.go
+++ test/official/expected/generate.go
@@ -1,4 +1,4 @@
-//go:generate go run ../../../cmd/asyncapi-codegen -p test -i ../spec/examples/simple.yml -o ./simple/simple.expected.go
-//go:generate go run ../../../cmd/asyncapi-codegen -p test -i ../spec/examples/correlation-id.yml -o ./correlation-id/correlation_id.expected.go
+//go:generate go run ../../../cmd/asyncapi-codegen -p simple -i ../spec/examples/simple.yml -o ./simple/simple.expected.go
+//go:generate go run ../../../cmd/asyncapi-codegen -p correlationID -i ../spec/examples/correlation-id.yml -o ./correlation-id/correlation_id.expected.go
 
 package expected
