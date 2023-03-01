f0b3394180e04e84f1afbebf398478952ea47f0e
Louis Fradin <lerenn@users.noreply.github.com>
2023-03-01 19:34:17 +0100

    Add disabling formatting option (#35)

--- pkg/codegen/errors.go
+++ pkg/codegen/errors.go
@@ -3,5 +3,9 @@ package codegen
 import "errors"
 
 var (
-	ErrInvalidFile = errors.New("invalid file")
+	// ErrInvalidBroker is an error raised when using an unknown broker
+	ErrInvalidBroker = errors.New("invalid broker")
+
+	// ErrInvalidFileFormat is returned when using an invalid format for AsyncAPI specification
+	ErrInvalidFileFormat = errors.New("invalid file format")
 )
--- pkg/codegen/options.go
+++ pkg/codegen/options.go
@@ -1,17 +1,21 @@
 package codegen
 
 import (
-	"errors"
-
 	"github.com/lerenn/asyncapi-codegen/pkg/codegen/generators"
 )
 
-var (
-	ErrInvalidBroker = errors.New("invalid broker")
-)
-
+// Options is the struct that gather configuration of codegen
 type Options struct {
-	OutputPath  string
+	// OutputPath is the path to the generated code file
+	OutputPath string
+
+	// PackageName is the package name of the generated code
 	PackageName string
-	Generate    generators.Options
+
+	// Generate contains options regarding which golang code should be generated
+	Generate generators.Options
+
+	// DisableFormatting states if the formatting should be disabled when
+	// writing the generated code
+	DisableFormatting bool
 }
