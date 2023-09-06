20c86ca9a6706181cdd40f3a6d8f23e80d3791c2
Olivier Bouchet <github@obo.aleeas.com>
2023-09-06 10:53:30 -0400

    feature: a basic kafka broker implementation (#68)

---
+++ examples/pingkafka/server/main.go
@@ -0,0 +1,60 @@
+//go:generate go run ../../../cmd/asyncapi-codegen -g application,types -p main -i ../asyncapi.yaml -o ./app.gen.go
+
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/lerenn/asyncapi-codegen/pkg/extensions/brokers"
+	"github.com/lerenn/asyncapi-codegen/pkg/extensions/loggers"
+)
+
+type ServerSubscriber struct {
+	Controller *AppController
+}
+
+func (s ServerSubscriber) Ping(ctx context.Context, req PingMessage, _ bool) {
+	// Generate a pong message, set as a response of the request
+	resp := NewPongMessage()
+	resp.SetAsResponseFrom(req)
+	resp.Payload.Message = "pong"
+	resp.Payload.Time = time.Now()
+
+	// Publish the pong message
+	// Note: it will indefinitely wait to publish as context has no timeout
+	err := s.Controller.PublishPong(ctx, resp)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	time.Sleep(5 * time.Second)
+
+	// Create a new client controller
+	host := "kafka:9092"
+	// Create a new server controller
+	ctrl, err := NewAppController(brokers.NewKafkaController([]string{host}))
+	if err != nil {
+		panic(err)
+	}
+	defer ctrl.Close(context.Background())
+
+	// Attach a logger (optional)
+	logger := loggers.NewECS()
+	ctrl.SetLogger(logger)
+
+	// Subscribe to all (we could also have just listened on the ping request channel)
+	sub := ServerSubscriber{Controller: ctrl}
+	if err := ctrl.SubscribeAll(context.Background(), sub); err != nil {
+		panic(err)
+	}
+
+	// Process messages until interruption signal
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+}
---
+++ pkg/extensions/brokers/kafka.go
@@ -0,0 +1,156 @@
+package brokers
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/lerenn/asyncapi-codegen/pkg/extensions"
+	"github.com/segmentio/kafka-go"
+)
+
+// KafkaController is the Kafka implementation for asyncapi-codegen
+type KafkaController struct {
+	logger    extensions.Logger
+	queueName string
+	hosts     []string
+	groupID   string
+	partition int
+	maxBytes  int
+}
+
+type KafkaControllerOption func(controller *KafkaController)
+
+// NewKafkaController creates a new KafkaController that fulfill the BrokerLinker interface
+func NewKafkaController(hosts []string, options ...KafkaControllerOption) *KafkaController {
+	controller := &KafkaController{
+		queueName: "asyncapi",
+		hosts:     hosts,
+		partition: 0,
+		maxBytes:  10e6, // 10MB
+	}
+	for _, option := range options {
+		option(controller)
+	}
+	return controller
+}
+
+func WithGroupID(groupID string) KafkaControllerOption {
+	return func(controller *KafkaController) {
+		controller.groupID = groupID
+	}
+}
+
+func WithPartition(partition int) KafkaControllerOption {
+	return func(controller *KafkaController) {
+		controller.partition = partition
+	}
+}
+
+func WithMaxBytes(maxBytes int) KafkaControllerOption {
+	return func(controller *KafkaController) {
+		controller.maxBytes = maxBytes
+	}
+}
+
+// SetQueueName sets a custom queue name for channel subscription
+//
+// It can be used for multiple applications listening one the same channel but
+// wants to listen on different queues.
+func (c *KafkaController) SetQueueName(name string) {
+	c.queueName = name
+}
+
+// SetLogger set a custom logger that will log operations on broker controller
+func (c *KafkaController) SetLogger(logger extensions.Logger) {
+	c.logger = logger
+}
+
+// Publish a message to the broker
+func (c *KafkaController) Publish(ctx context.Context, channel string, um extensions.BrokerMessage) error {
+	w := &kafka.Writer{
+		Addr:                   kafka.TCP(c.hosts...),
+		Topic:                  channel,
+		Balancer:               &kafka.LeastBytes{},
+		AllowAutoTopicCreation: true,
+	}
+
+	msg := kafka.Message{
+		Headers: make([]kafka.Header, 0),
+	}
+
+	// Set message content
+	msg.Value = um.Payload
+	if um.CorrelationID != nil {
+		msg.Headers = append(msg.Headers, kafka.Header{
+			Key:   correlationIDField,
+			Value: []byte(*um.CorrelationID),
+		})
+	}
+
+	// Publish message
+	if err := w.WriteMessages(ctx, msg); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Subscribe to messages from the broker
+func (c *KafkaController) Subscribe(ctx context.Context, channel string) (msgs chan extensions.BrokerMessage, stop chan interface{}, err error) {
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:   c.hosts,
+		Topic:     channel,
+		Partition: c.partition,
+		MaxBytes:  c.maxBytes,
+	})
+
+	getHeaders := func(msg kafka.Message, key string) string {
+		for _, header := range msg.Headers {
+			if header.Key == key {
+				return string(header.Value)
+			}
+		}
+		return ""
+	}
+
+	// Handle events
+	msgs = make(chan extensions.BrokerMessage, 64)
+	stop = make(chan interface{}, 1)
+	go func() {
+		for {
+			msg, err := r.ReadMessage(ctx)
+			if err != nil {
+				break
+			}
+			var correlationID *string
+
+			// Add correlation ID if not empty
+			str := getHeaders(msg, correlationIDField)
+			if str != "" {
+				correlationID = &str
+			}
+
+			// Create message
+			msgs <- extensions.BrokerMessage{
+				Payload:       msg.Value,
+				CorrelationID: correlationID,
+			}
+		}
+	}()
+
+	go func() {
+		for { // nolint
+			select {
+			// Handle closure request from function caller
+			case _ = <-stop: // nolint
+				fmt.Print("Stopping subscriber")
+				if err := r.Close(); err != nil && c.logger != nil {
+					c.logger.Error(ctx, err.Error())
+				}
+				close(msgs)
+			}
+		}
+	}()
+
+	return msgs, stop, nil
+}
