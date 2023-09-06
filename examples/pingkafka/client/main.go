987f5a2a9ede7f857c6fe2fffa14a1d134e47421
Olivier Bouchet <obouchet@gmail.com>
2023-09-06 08:12:13 -0400

    feature: a basic kafka broker implementation

---
+++ examples/pingkafka/client/main.go
@@ -0,0 +1,56 @@
+//go:generate go run ../../../cmd/asyncapi-codegen -g client,types -p main -i ../asyncapi.yaml -o ./client.gen.go
+
+package main
+
+import (
+	"context"
+	"time"
+
+	"github.com/lerenn/asyncapi-codegen/pkg/extensions/brokers"
+	"github.com/lerenn/asyncapi-codegen/pkg/extensions/loggers"
+	"github.com/lerenn/asyncapi-codegen/pkg/extensions/middlewares"
+)
+
+func main() {
+	time.Sleep(6 * time.Second)
+
+	// Create a new client controller
+	host := "kafka:9092"
+	ctrl, err := NewClientController(brokers.NewKafkaController([]string{host}))
+	if err != nil {
+		panic(err)
+	}
+	defer ctrl.Close(context.Background())
+
+	// Attach a logger (optional)
+	logger := loggers.NewECS()
+	ctrl.SetLogger(logger)
+	ctrl.AddMiddlewares(middlewares.Logging(logger))
+
+	// Make a new ping message
+	req := NewPingMessage()
+	req.Payload = "ping"
+
+	// Create the publication function to send the message
+	// Note: it will indefinitely wait to publish as context has no timeout
+	publicationFunc := func(ctx context.Context) error {
+		return ctrl.PublishPing(ctx, req)
+	}
+
+	// The following function will subscribe to the 'pong' channel, execute the publication
+	// function and wait for a response. The response will be detected through its
+	// correlation ID.
+	//
+	// This function is available only if the 'correlationId' field has been filled
+	// for any channel in the AsyncAPI specification. You will then be able to use it
+	// with the form WaitForXXX where XXX is the channel name.
+	//
+	// Note: it will indefinitely wait for messages as context has no timeout
+	_, err = ctrl.WaitForPong(context.Background(), req, publicationFunc)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the message to be received
+	time.Sleep(time.Second)
+}
--- go.mod
+++ go.mod
@@ -4,25 +4,28 @@ go 1.19
 
 require (
 	github.com/ghodss/yaml v1.0.0
-	github.com/google/uuid v1.3.0
+	github.com/google/uuid v1.3.1
 	github.com/matryer/is v1.4.1
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826
-	github.com/nats-io/nats.go v1.23.0
-	github.com/stoewer/go-strcase v1.2.1
-	github.com/stretchr/testify v1.8.1
-	golang.org/x/tools v0.5.0
+	github.com/nats-io/nats.go v1.28.0
+	github.com/segmentio/kafka-go v0.4.42
+	github.com/stoewer/go-strcase v1.3.0
+	github.com/stretchr/testify v1.8.4
+	golang.org/x/tools v0.13.0
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
 	github.com/nats-io/nats-server/v2 v2.9.11 // indirect
-	github.com/nats-io/nkeys v0.3.0 // indirect
+	github.com/nats-io/nkeys v0.4.4 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.5.0 // indirect
-	golang.org/x/mod v0.7.0 // indirect
-	golang.org/x/sys v0.4.0 // indirect
+	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
