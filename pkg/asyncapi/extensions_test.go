a526273cfd4e9b040daaa98f4cf3caaacbdfc7b5
Louis FRADIN <louis.fradin@gmail.com>
2023-09-13 20:12:14 +0400

    add check on function length

--- pkg/asyncapi/extensions_test.go
+++ pkg/asyncapi/extensions_test.go
@@ -15,59 +15,12 @@ type ExtensionsSuite struct {
 	suite.Suite
 }
 
-func (suite *ExtensionsSuite) TestExtGoTypeImport() {
-	tests := []struct {
-		name     string
-		schema   *Any
-		expected []string
-		error    bool
-	}{
-		// Schema
-		{
-			name: "flag",
-			schema: &Any{
-				Type: TypeIsInteger.String(),
-				Extensions: Extensions{
-					ExtGoType: "mypackage.Flag",
-					ExtGoTypeImport: &GoTypeImportExtension{
-						Path: "abc.xyz/repo/mypackage",
-					},
-				},
-			},
-			expected: []string{
-				`"abc.xyz/repo/mypackage"`,
-			},
-		},
-
-		// Object property, default name
-		{
-			name: TypeIsObject.String(),
-			schema: &Any{
-				Type: TypeIsObject.String(),
-				Properties: map[string]*Any{
-					"flag": {
-						Type: TypeIsInteger.String(),
-						Extensions: Extensions{
-							ExtGoType: "mypackage.Flag",
-							ExtGoTypeImport: &GoTypeImportExtension{
-								Path: "abc.xyz/repo/mypackage",
-							},
-						},
-					},
-				},
-				Required: []string{"flag"},
-			},
-			expected: []string{
-				`"abc.xyz/repo/mypackage"`,
-			},
-		},
-
-		// Array item
-		{
-			name: "flags",
-			schema: &Any{
-				Type: TypeIsArray.String(),
-				Items: &Any{
+func (suite *ExtensionsSuite) TextExtGoTypeImportWithSchema() {
+	// Set specification
+	spec := Specification{
+		Components: Components{
+			Schemas: map[string]*Any{
+				"flag": {
 					Type: TypeIsInteger.String(),
 					Extensions: Extensions{
 						ExtGoType: "mypackage.Flag",
@@ -77,119 +30,196 @@ func (suite *ExtensionsSuite) TestExtGoTypeImport() {
 					},
 				},
 			},
-			expected: []string{
-				`"abc.xyz/repo/mypackage"`,
-			},
-		},
-
-		// Object property, change package name
-		{
-			name: TypeIsObject.String(),
-			schema: &Any{
-				Type: TypeIsObject.String(),
-				Properties: map[string]*Any{
-					"flag": {
-						Type: TypeIsInteger.String(),
-						Extensions: Extensions{
-							ExtGoType: "alias.Flag",
-							ExtGoTypeImport: &GoTypeImportExtension{
-								Path: "abc.xyz/repo/mypackage",
-								Name: "alias",
-							},
-						},
-					},
-				},
-				Required: []string{"flag"},
-			},
-			expected: []string{
-				`alias "abc.xyz/repo/mypackage"`,
-			},
-		},
-
-		// Object property, multiple imports
-		{
-			name: TypeIsObject.String(),
-			schema: &Any{
-				Type: TypeIsObject.String(),
-				Properties: map[string]*Any{
-					"flag": {
-						Type: TypeIsInteger.String(),
-						Extensions: Extensions{
-							ExtGoType: "alias.Flag",
-							ExtGoTypeImport: &GoTypeImportExtension{
-								Path: "abc.xyz/repo/mypackage",
-								Name: "alias",
-							},
-						},
-					},
-					"id": {
-						Type: TypeIsString.String(),
-						Extensions: Extensions{
-							ExtGoType: "xid.ID",
-							ExtGoTypeImport: &GoTypeImportExtension{
-								Path: "github.com/rs/xid",
-							},
-						},
-					},
-				},
-				Required: []string{"flag"},
-			},
-			expected: []string{
-				`alias "abc.xyz/repo/mypackage"`,
-				`"github.com/rs/xid"`,
-			},
-		},
-
-		// Conflicting import package name will error
-		{
-			name: TypeIsObject.String(),
-			schema: &Any{
-				Type: TypeIsObject.String(),
-				Properties: map[string]*Any{
-					"start_flag": {
-						Type: TypeIsInteger.String(),
-						Extensions: Extensions{
-							ExtGoType: "alias2.Flag",
-							ExtGoTypeImport: &GoTypeImportExtension{
-								Path: "abc.xyz/repo/mypackage",
-								Name: "alias1",
-							},
-						},
-					},
-					"end_flag": {
-						Type: TypeIsInteger.String(),
-						Extensions: Extensions{
-							ExtGoType: "alias2.Flag",
-							ExtGoTypeImport: &GoTypeImportExtension{
-								Path: "abc.xyz/repo/mypackage",
-								Name: "alias2",
-							},
-						},
-					},
-				},
-				Required: []string{"start_flag", "end_flag"},
-			},
-			error: true,
 		},
 	}
 
-	for _, test := range tests {
-		spec := Specification{
-			Components: Components{
-				Schemas: map[string]*Any{test.name: test.schema},
+	// Process custom imports
+	res, err := spec.CustomImports()
+	suite.Require().NoError(err)
+
+	// Check result
+	sort.Strings(res)
+	suite.Require().Equal([]string{`"abc.xyz/repo/mypackage"`}, res)
+}
+
+func (suite *ExtensionsSuite) TextExtGoTypeImportWithObjectProperty() {
+	// Set specification
+	spec := Specification{
+		Components: Components{
+			Schemas: map[string]*Any{
+				TypeIsObject.String(): {
+					Type: TypeIsObject.String(),
+					Properties: map[string]*Any{
+						"flag": {
+							Type: TypeIsInteger.String(),
+							Extensions: Extensions{
+								ExtGoType: "mypackage.Flag",
+								ExtGoTypeImport: &GoTypeImportExtension{
+									Path: "abc.xyz/repo/mypackage",
+								},
+							},
+						},
+					},
+					Required: []string{"flag"},
+				},
 			},
-		}
-
-		res, err := spec.CustomImports()
-
-		if test.error {
-			suite.Require().True(err != nil)
-		} else {
-			suite.Require().NoError(err)
-
-			sort.Strings(test.expected)
-			sort.Strings(res)
-			suite.Require().Equal(res, test.expected)
-		}
+		},
 	}
+
+	// Process custom imports
+	res, err := spec.CustomImports()
+	suite.Require().NoError(err)
+
+	// Check result
+	sort.Strings(res)
+	suite.Require().Equal([]string{`"abc.xyz/repo/mypackage"`}, res)
+}
+
+func (suite *ExtensionsSuite) TextExtGoTypeImportWithArrayItem() {
+	// Set specification
+	spec := Specification{
+		Components: Components{
+			Schemas: map[string]*Any{
+				"flag": {
+					Type: TypeIsArray.String(),
+					Items: &Any{
+						Type: TypeIsInteger.String(),
+						Extensions: Extensions{
+							ExtGoType: "mypackage.Flag",
+							ExtGoTypeImport: &GoTypeImportExtension{
+								Path: "abc.xyz/repo/mypackage",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Process custom imports
+	res, err := spec.CustomImports()
+	suite.Require().NoError(err)
+
+	// Check result
+	sort.Strings(res)
+	suite.Require().Equal([]string{`"abc.xyz/repo/mypackage"`}, res)
+}
+
+func (suite *ExtensionsSuite) TextExtGoTypeImportWithObjectPropertyAndDifferentPackageName() {
+	// Set specification
+	spec := Specification{
+		Components: Components{
+			Schemas: map[string]*Any{
+				TypeIsObject.String(): {
+					Type: TypeIsObject.String(),
+					Properties: map[string]*Any{
+						"flag": {
+							Type: TypeIsInteger.String(),
+							Extensions: Extensions{
+								ExtGoType: "alias.Flag",
+								ExtGoTypeImport: &GoTypeImportExtension{
+									Path: "abc.xyz/repo/mypackage",
+									Name: "alias",
+								},
+							},
+						},
+					},
+					Required: []string{"flag"},
+				},
+			},
+		},
+	}
+
+	// Process custom imports
+	res, err := spec.CustomImports()
+	suite.Require().NoError(err)
+
+	// Check result
+	sort.Strings(res)
+	suite.Require().Equal([]string{`alias "abc.xyz/repo/mypackage"`}, res)
+}
+
+func (suite *ExtensionsSuite) TextExtGoTypeImportWithObjectPropertyAndMultipleImports() {
+	// Set specification
+	spec := Specification{
+		Components: Components{
+			Schemas: map[string]*Any{
+				TypeIsObject.String(): {
+					Type: TypeIsObject.String(),
+					Properties: map[string]*Any{
+						"flag": {
+							Type: TypeIsInteger.String(),
+							Extensions: Extensions{
+								ExtGoType: "alias.Flag",
+								ExtGoTypeImport: &GoTypeImportExtension{
+									Path: "abc.xyz/repo/mypackage",
+									Name: "alias",
+								},
+							},
+						},
+						"id": {
+							Type: TypeIsString.String(),
+							Extensions: Extensions{
+								ExtGoType: "xid.ID",
+								ExtGoTypeImport: &GoTypeImportExtension{
+									Path: "github.com/rs/xid",
+								},
+							},
+						},
+					},
+					Required: []string{"flag"},
+				},
+			},
+		},
+	}
+
+	// Process custom imports
+	res, err := spec.CustomImports()
+	suite.Require().NoError(err)
+
+	// Check result
+	sort.Strings(res)
+	suite.Require().Equal([]string{`alias "abc.xyz/repo/mypackage"`, `"github.com/rs/xid"`}, res)
+}
+
+func (suite *ExtensionsSuite) TextExtGoTypeImportWithConflictingPackageNames() {
+	// Set specification
+	spec := Specification{
+		Components: Components{
+			Schemas: map[string]*Any{
+				TypeIsObject.String(): {
+					Type: TypeIsObject.String(),
+					Properties: map[string]*Any{
+						"start_flag": {
+							Type: TypeIsInteger.String(),
+							Extensions: Extensions{
+								ExtGoType: "alias2.Flag",
+								ExtGoTypeImport: &GoTypeImportExtension{
+									Path: "abc.xyz/repo/mypackage",
+									Name: "alias1",
+								},
+							},
+						},
+						"end_flag": {
+							Type: TypeIsInteger.String(),
+							Extensions: Extensions{
+								ExtGoType: "alias2.Flag",
+								ExtGoTypeImport: &GoTypeImportExtension{
+									Path: "abc.xyz/repo/mypackage",
+									Name: "alias2",
+								},
+							},
+						},
+					},
+					Required: []string{"start_flag", "end_flag"},
+				},
+			},
+		},
+	}
+
+	// Process custom imports
+	_, err := spec.CustomImports()
+
+	// It should be an error
+	suite.Require().Error(err)
 }
