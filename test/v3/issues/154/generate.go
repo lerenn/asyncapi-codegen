89541fc809517dde6430f2e3533483a287be7c98
Louis FRADIN <louis.fradin@gmail.com>
2024-03-13 23:26:55 +0100

    fix and test

---
+++ test/v3/issues/154/generate.go
@@ -0,0 +1,5 @@
+//go:generate go run ../../../../cmd/asyncapi-codegen -p issue154 -g types -i ./asyncapi.yaml -o ./asyncapi.gen.go
+
+package issue154
+
+// This is just to test that the generation is correct
