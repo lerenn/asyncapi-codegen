3c7c225c880b1c7e8de9265b26840079a55e1d39
Louis Fradin <lerenn@users.noreply.github.com>
2024-03-17 20:11:06 +0100

    ci(docker): add multi-arch support (#160)

--- pkg/ci/publish.go
+++ pkg/ci/publish.go
@@ -12,6 +12,20 @@ const (
 	DockerImageName = "lerenn/asyncapi-codegen"
 )
 
+var (
+	// platforms represents the different OS/Arch platform wanted for docker hub.
+	platforms = []RunnerType{
+		{OS: "linux", Arch: "386", BuildBaseImage: "golang:alpine", TargetBaseImage: "alpine"},
+		{OS: "linux", Arch: "amd64", BuildBaseImage: "golang:alpine", TargetBaseImage: "alpine"},
+		{OS: "linux", Arch: "arm/v6", BuildBaseImage: "golang:alpine", TargetBaseImage: "alpine"},
+		{OS: "linux", Arch: "arm/v7", BuildBaseImage: "golang:alpine", TargetBaseImage: "alpine"},
+		{OS: "linux", Arch: "arm64/v8", BuildBaseImage: "golang:alpine", TargetBaseImage: "alpine"},
+		{OS: "linux", Arch: "mips64le", BuildBaseImage: "golang", TargetBaseImage: "debian:12"},
+		{OS: "linux", Arch: "ppc64le", BuildBaseImage: "golang:alpine", TargetBaseImage: "alpine"},
+		{OS: "linux", Arch: "s390x", BuildBaseImage: "golang:alpine", TargetBaseImage: "alpine"},
+	}
+)
+
 // Publish should publish tag on git repository and docker image(s) on Docker Hub
 // Note: if this is not 'main' branch, then it will just push docker image with
 // git tag.
@@ -45,14 +59,25 @@ func tagAndPush(tag string) error {
 }
 
 func publishDocker(ctx context.Context, client *dagger.Client, tag string) error {
-	runner := RunnerFromDockerfile(client)
+	// Get images for each platform
+	platformVariants := make([]*dagger.Container, len(platforms))
+	for i, p := range platforms {
+		platformVariants[i] = RunnerFromDockerfile(client, p)
+	}
 
-	// Publish with git commit hash as tag
+	// Set publication options from images
+	publishOpts := dagger.ContainerPublishOpts{
+		PlatformVariants: platformVariants,
+	}
+
+	// Get last git commit hash
 	hash, err := git.GetLastCommitHash(".")
 	if err != nil {
 		return err
 	}
-	if _, err := runner.Publish(ctx, DockerImageName+":"+hash); err != nil {
+
+	// Publish with hash
+	if _, err := client.Container().Publish(ctx, DockerImageName+":"+hash, publishOpts); err != nil {
 		return err
 	}
 
@@ -63,12 +88,15 @@ func publishDocker(ctx context.Context, client *dagger.Client, tag string) error
 		return nil
 	}
 
-	// Publish with tag
-	if _, err = runner.Publish(ctx, DockerImageName+":"+tag); err != nil {
+	// Publish with tag passed in argument
+	if _, err := client.Container().Publish(ctx, DockerImageName+":"+tag, publishOpts); err != nil {
 		return err
 	}
 
-	// Publish as latest
-	_, err = runner.Publish(ctx, DockerImageName+":latest")
-	return err
+	// Publish with "latest" as tag
+	if _, err := client.Container().Publish(ctx, DockerImageName+":latest", publishOpts); err != nil {
+		return err
+	}
+
+	return nil
 }
--- pkg/ci/runner.go
+++ pkg/ci/runner.go
@@ -1,10 +1,46 @@
 package ci
 
-import "dagger.io/dagger"
+import (
+	"runtime"
+
+	"dagger.io/dagger"
+)
+
+// RunnerType represents the type of runner based on various info.
+type RunnerType struct {
+	OS              string
+	Arch            string
+	BuildBaseImage  string
+	TargetBaseImage string
+}
+
+var (
+	// DefaultRunnerType is the default runner type to use in case of doubt.
+	DefaultRunnerType = RunnerType{
+		OS:              "linux",
+		Arch:            "amd64",
+		BuildBaseImage:  "golang:alpine",
+		TargetBaseImage: "alpine",
+	}
+)
 
 // RunnerFromDockerfile returns a dagger container based on the repository Dockerfile.
-func RunnerFromDockerfile(client *dagger.Client) *dagger.Container {
+func RunnerFromDockerfile(client *dagger.Client, rt RunnerType) *dagger.Container {
+	// Get running OS, if that's an OS unsupported by Docker, replace by Linux
+	os := runtime.GOOS
+	if os == "darwin" {
+		os = "linux"
+	}
+
 	return client.Host().Directory(".").DockerBuild(dagger.DirectoryDockerBuildOpts{
+		BuildArgs: []dagger.BuildArg{
+			{Name: "BUILDPLATFORM", Value: os + "/" + runtime.GOARCH},
+			{Name: "TARGETOS", Value: rt.OS},
+			{Name: "TARGETARCH", Value: rt.Arch},
+			{Name: "BUILDBASEIMAGE", Value: rt.BuildBaseImage},
+			{Name: "TARGETBASEIMAGE", Value: rt.TargetBaseImage},
+		},
+		Platform:   dagger.Platform(rt.OS + "/" + rt.Arch),
 		Dockerfile: "/build/package/Dockerfile",
 	})
 }
