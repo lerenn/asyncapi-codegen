3703db6856a700b90ef546c04af7325e0156b0d4
magraef <mario.graef@posteo.de>
2024-03-26 23:45:20 +0100

    * remove cycling import to errorhandlers

--- pkg/extensions/errorhandler.go
+++ pkg/extensions/errorhandler.go
@@ -2,14 +2,13 @@ package extensions
 
 import (
 	"context"
-	"github.com/lerenn/asyncapi-codegen/pkg/extensions/errorhandlers"
 )
 
 // ErrorHandler is the signature of the function that needs to be implemented to use
 // errorhandler functionality
 type ErrorHandler func(ctx context.Context, topic string, msg *AcknowledgeableBrokerMessage, err error)
 
-// DefaultErrorHandler returns the default error handler, which is Noop one.
+// DefaultErrorHandler returns the default error handler, which is a Noop errorhandler.
 func DefaultErrorHandler() ErrorHandler {
-	return errorhandlers.Noop()
+	return func(ctx context.Context, topic string, msg *AcknowledgeableBrokerMessage, err error) {}
 }
