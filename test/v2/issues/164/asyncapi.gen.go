eed310c3be5631d34307910e3457b9175f576e56
Matouš Dzivjak <matousdzivjak@gmail.com>
2024-04-04 21:55:11 +0200

    fix(generators): v3 marshalling with additional properties
    Fix generated code for marshalling additional properties.
    The test case for the behaviour relied on a harcoded struct name
    that should have used the templated name of the generated struct
    instead.

--- test/v2/issues/164/asyncapi.gen.go
+++ test/v2/issues/164/asyncapi.gen.go
@@ -474,7 +474,7 @@ type TestMapSchema struct {
 	AdditionalProperties map[string]string `json:"-"`
 }
 
-// MarshalJSON will override the marshal as this is not a normal 'time.Time' type
+// MarshalJSON marshals the schema into JSON with support for additional properties.
 func (t TestMapSchema) MarshalJSON() ([]byte, error) {
 	type alias TestMapSchema
 
@@ -496,7 +496,7 @@ func (t TestMapSchema) MarshalJSON() ([]byte, error) {
 	return append(b, []byte("}")...), nil
 }
 
-// UnmarshalJSON will override the unmarshal as this is not a normal 'time.Time' type
+// UnmarshalJSON unmarshals schema from JSON with support for additional properties.
 func (t *TestMapSchema) UnmarshalJSON(data []byte) error {
 	type alias TestMapSchema
 
