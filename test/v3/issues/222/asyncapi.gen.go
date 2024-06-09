cf40709d998f5920f05942f92948be9f557a9693
Louis FRADIN <louis.fradin@gmail.com>
2024-06-09 14:38:52 +0200

    fix test for v3

--- test/v3/issues/222/asyncapi.gen.go
+++ test/v3/issues/222/asyncapi.gen.go
@@ -14,7 +14,13 @@ import (
 	"cloud.google.com/go/civil"
 )
 
-// AppController is the structure that provides publishing capabilities to the
+// AppSubscriber contains all handlers that are listening messages for App
+type AppSubscriber interface {
+	// HandleTestingOperationReceived receive all TestMessageMessageFromTestingChannel messages from Testing channel.
+	HandleTestingOperationReceived(ctx context.Context, msg TestMessageMessageFromTestingChannel) error
+}
+
+// AppController is the structure that provides sending capabilities to the
 // developer and and connect the broker with the App
 type AppController struct {
 	controller
@@ -101,51 +107,146 @@ func (c AppController) executeMiddlewares(ctx context.Context, msg *extensions.B
 	return wrapped(ctx, msg)
 }
 
-func addAppContextValues(ctx context.Context, path string) context.Context {
+func addAppContextValues(ctx context.Context, addr string) context.Context {
 	ctx = context.WithValue(ctx, extensions.ContextKeyIsVersion, "1.2.3")
 	ctx = context.WithValue(ctx, extensions.ContextKeyIsProvider, "app")
-	return context.WithValue(ctx, extensions.ContextKeyIsChannel, path)
+	return context.WithValue(ctx, extensions.ContextKeyIsChannel, addr)
 }
 
 // Close will clean up any existing resources on the controller
 func (c *AppController) Close(ctx context.Context) {
 	// Unsubscribing remaining channels
+	c.UnsubscribeFromAllChannels(ctx)
+
+	c.logger.Info(ctx, "Closed app controller")
 }
 
-// PublishV3Issue222Test will publish messages to 'v3.issue222.test' channel
-func (c *AppController) PublishV3Issue222Test(
-	ctx context.Context,
-	msg V3Issue222TestMessage,
-) error {
-	// Get channel path
-	path := "v3.issue222.test"
+// SubscribeToAllChannels will receive messages from channels where channel has
+// no parameter on which the app is expecting messages. For channels with parameters,
+// they should be subscribed independently.
+func (c *AppController) SubscribeToAllChannels(ctx context.Context, as AppSubscriber) error {
+	if as == nil {
+		return extensions.ErrNilAppSubscriber
+	}
 
-	// Set context
-	ctx = addAppContextValues(ctx, path)
-	ctx = context.WithValue(ctx, extensions.ContextKeyIsDirection, "publication")
-
-	// Convert to BrokerMessage
-	brokerMsg, err := msg.toBrokerMessage()
-	if err != nil {
+	if err := c.SubscribeToHandleTestingOperation(ctx, as.HandleTestingOperationReceived); err != nil {
 		return err
 	}
 
-	// Set broker message to context
-	ctx = context.WithValue(ctx, extensions.ContextKeyIsBrokerMessage, brokerMsg.String())
-
-	// Publish the message on event-broker through middlewares
-	return c.executeMiddlewares(ctx, &brokerMsg, func(ctx context.Context) error {
-		return c.broker.Publish(ctx, path, brokerMsg)
-	})
+	return nil
 }
 
-// UserSubscriber represents all handlers that are expecting messages for User
-type UserSubscriber interface {
-	// V3Issue222Test subscribes to messages placed on the 'v3.issue222.test' channel
-	V3Issue222Test(ctx context.Context, msg V3Issue222TestMessage) error
+// UnsubscribeFromAllChannels will stop the subscription of all remaining subscribed channels
+func (c *AppController) UnsubscribeFromAllChannels(ctx context.Context) {
+	c.UnsubscribeFromHandleTestingOperation(ctx)
 }
 
-// UserController is the structure that provides publishing capabilities to the
+// SubscribeToHandleTestingOperation will receive TestMessageMessageFromTestingChannel messages from Testing channel.
+//
+// Callback function 'fn' will be called each time a new message is received.
+//
+// NOTE: for now, this only support the first message from AsyncAPI list.
+//
+// NOTE: for now, this only support the first message from AsyncAPI list.
+// If you need support for other messages, please raise an issue.
+func (c *AppController) SubscribeToHandleTestingOperation(
+	ctx context.Context,
+	fn func(ctx context.Context, msg TestMessageMessageFromTestingChannel) error,
+) error {
+	// Get channel address
+	addr := "v3.issue222.test"
+
+	// Set context
+	ctx = addAppContextValues(ctx, addr)
+	ctx = context.WithValue(ctx, extensions.ContextKeyIsDirection, "reception")
+
+	// Check if the controller is already subscribed
+	_, exists := c.subscriptions[addr]
+	if exists {
+		err := fmt.Errorf("%w: controller is already subscribed on channel %q", extensions.ErrAlreadySubscribedChannel, addr)
+		c.logger.Error(ctx, err.Error())
+		return err
+	}
+
+	// Subscribe to broker channel
+	sub, err := c.broker.Subscribe(ctx, addr)
+	if err != nil {
+		c.logger.Error(ctx, err.Error())
+		return err
+	}
+	c.logger.Info(ctx, "Subscribed to channel")
+
+	// Asynchronously listen to new messages and pass them to app receiver
+	go func() {
+		for {
+			// Wait for next message
+			acknowledgeableBrokerMessage, open := <-sub.MessagesChannel()
+
+			// If subscription is closed and there is no more message
+			// (i.e. uninitialized message), then exit the function
+			if !open && acknowledgeableBrokerMessage.IsUninitialized() {
+				return
+			}
+
+			// Set broker message to context
+			ctx = context.WithValue(ctx, extensions.ContextKeyIsBrokerMessage, acknowledgeableBrokerMessage.String())
+
+			// Execute middlewares before handling the message
+			if err := c.executeMiddlewares(ctx, &acknowledgeableBrokerMessage.BrokerMessage, func(ctx context.Context) error {
+				// Process message
+				msg, err := brokerMessageToTestMessageMessageFromTestingChannel(acknowledgeableBrokerMessage.BrokerMessage)
+				if err != nil {
+					return err
+				}
+
+				// Execute the subscription function
+				if err := fn(ctx, msg); err != nil {
+					return err
+				}
+
+				acknowledgeableBrokerMessage.Ack()
+
+				return nil
+			}); err != nil {
+				c.errorHandler(ctx, addr, &acknowledgeableBrokerMessage, err)
+				// On error execute the acknowledgeableBrokerMessage nack() function and
+				// let the BrokerAcknowledgment decide what is the right nack behavior for the broker
+				acknowledgeableBrokerMessage.Nak()
+			}
+		}
+	}()
+
+	// Add the cancel channel to the inside map
+	c.subscriptions[addr] = sub
+
+	return nil
+} // UnsubscribeFromHandleTestingOperation will stop the reception of TestMessageMessageFromTestingChannel messages from Testing channel.
+// A timeout can be set in context to avoid blocking operation, if needed.
+func (c *AppController) UnsubscribeFromHandleTestingOperation(
+	ctx context.Context,
+) {
+	// Get channel address
+	addr := "v3.issue222.test"
+
+	// Check if there receivers for this channel
+	sub, exists := c.subscriptions[addr]
+	if !exists {
+		return
+	}
+
+	// Set context
+	ctx = addAppContextValues(ctx, addr)
+
+	// Stop the subscription
+	sub.Cancel(ctx)
+
+	// Remove if from the receivers
+	delete(c.subscriptions, addr)
+
+	c.logger.Info(ctx, "Unsubscribed from channel")
+}
+
+// UserController is the structure that provides sending capabilities to the
 // developer and and connect the broker with the User
 type UserController struct {
 	controller
@@ -232,137 +333,45 @@ func (c UserController) executeMiddlewares(ctx context.Context, msg *extensions.
 	return wrapped(ctx, msg)
 }
 
-func addUserContextValues(ctx context.Context, path string) context.Context {
+func addUserContextValues(ctx context.Context, addr string) context.Context {
 	ctx = context.WithValue(ctx, extensions.ContextKeyIsVersion, "1.2.3")
 	ctx = context.WithValue(ctx, extensions.ContextKeyIsProvider, "user")
-	return context.WithValue(ctx, extensions.ContextKeyIsChannel, path)
+	return context.WithValue(ctx, extensions.ContextKeyIsChannel, addr)
 }
 
 // Close will clean up any existing resources on the controller
 func (c *UserController) Close(ctx context.Context) {
 	// Unsubscribing remaining channels
-	c.UnsubscribeAll(ctx)
-
-	c.logger.Info(ctx, "Closed user controller")
 }
 
-// SubscribeAll will subscribe to channels without parameters on which the app is expecting messages.
-// For channels with parameters, they should be subscribed independently.
-func (c *UserController) SubscribeAll(ctx context.Context, as UserSubscriber) error {
-	if as == nil {
-		return extensions.ErrNilUserSubscriber
-	}
-
-	if err := c.SubscribeV3Issue222Test(ctx, as.V3Issue222Test); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// UnsubscribeAll will unsubscribe all remaining subscribed channels
-func (c *UserController) UnsubscribeAll(ctx context.Context) {
-	c.UnsubscribeV3Issue222Test(ctx)
-}
-
-// SubscribeV3Issue222Test will subscribe to new messages from 'v3.issue222.test' channel.
+// SendToHandleTestingOperation will send a TestMessageMessageFromTestingChannel message on Testing channel.
 //
-// Callback function 'fn' will be called each time a new message is received.
-func (c *UserController) SubscribeV3Issue222Test(
+// NOTE: for now, this only support the first message from AsyncAPI list.
+// If you need support for other messages, please raise an issue.
+func (c *UserController) SendToHandleTestingOperation(
 	ctx context.Context,
-	fn func(ctx context.Context, msg V3Issue222TestMessage) error,
+	msg TestMessageMessageFromTestingChannel,
 ) error {
-	// Get channel path
-	path := "v3.issue222.test"
+	// Set channel address
+	addr := "v3.issue222.test"
 
 	// Set context
-	ctx = addUserContextValues(ctx, path)
-	ctx = context.WithValue(ctx, extensions.ContextKeyIsDirection, "reception")
+	ctx = addUserContextValues(ctx, addr)
+	ctx = context.WithValue(ctx, extensions.ContextKeyIsDirection, "publication")
 
-	// Check if there is already a subscription
-	_, exists := c.subscriptions[path]
-	if exists {
-		err := fmt.Errorf("%w: %q channel is already subscribed", extensions.ErrAlreadySubscribedChannel, path)
-		c.logger.Error(ctx, err.Error())
-		return err
-	}
-
-	// Subscribe to broker channel
-	sub, err := c.broker.Subscribe(ctx, path)
+	// Convert to BrokerMessage
+	brokerMsg, err := msg.toBrokerMessage()
 	if err != nil {
-		c.logger.Error(ctx, err.Error())
 		return err
 	}
-	c.logger.Info(ctx, "Subscribed to channel")
 
-	// Asynchronously listen to new messages and pass them to app subscriber
-	go func() {
-		for {
-			// Wait for next message
-			acknowledgeableBrokerMessage, open := <-sub.MessagesChannel()
+	// Set broker message to context
+	ctx = context.WithValue(ctx, extensions.ContextKeyIsBrokerMessage, brokerMsg.String())
 
-			// If subscription is closed and there is no more message
-			// (i.e. uninitialized message), then exit the function
-			if !open && acknowledgeableBrokerMessage.IsUninitialized() {
-				return
-			}
-
-			// Set broker message to context
-			ctx = context.WithValue(ctx, extensions.ContextKeyIsBrokerMessage, acknowledgeableBrokerMessage.String())
-
-			// Execute middlewares before handling the message
-			if err := c.executeMiddlewares(ctx, &acknowledgeableBrokerMessage.BrokerMessage, func(ctx context.Context) error {
-				// Process message
-				msg, err := brokerMessageToV3Issue222TestMessage(acknowledgeableBrokerMessage.BrokerMessage)
-				if err != nil {
-					return err
-				}
-
-				// Execute the subscription function
-				if err := fn(ctx, msg); err != nil {
-					return err
-				}
-
-				acknowledgeableBrokerMessage.Ack()
-
-				return nil
-			}); err != nil {
-				c.errorHandler(ctx, path, &acknowledgeableBrokerMessage, err)
-				// On error execute the acknowledgeableBrokerMessage nack() function and
-				// let the BrokerAcknowledgment decide what is the right nack behavior for the broker
-				acknowledgeableBrokerMessage.Nak()
-			}
-		}
-	}()
-
-	// Add the cancel channel to the inside map
-	c.subscriptions[path] = sub
-
-	return nil
-}
-
-// UnsubscribeV3Issue222Test will unsubscribe messages from 'v3.issue222.test' channel.
-// A timeout can be set in context to avoid blocking operation, if needed.
-func (c *UserController) UnsubscribeV3Issue222Test(ctx context.Context) {
-	// Get channel path
-	path := "v3.issue222.test"
-
-	// Check if there subscribers for this channel
-	sub, exists := c.subscriptions[path]
-	if !exists {
-		return
-	}
-
-	// Set context
-	ctx = addUserContextValues(ctx, path)
-
-	// Stop the subscription
-	sub.Cancel(ctx)
-
-	// Remove if from the subscribers
-	delete(c.subscriptions, path)
-
-	c.logger.Info(ctx, "Unsubscribed from channel")
+	// Send the message on event-broker through middlewares
+	return c.executeMiddlewares(ctx, &brokerMsg, func(ctx context.Context) error {
+		return c.broker.Publish(ctx, addr, brokerMsg)
+	})
 }
 
 // AsyncAPIVersion is the version of the used AsyncAPI document
@@ -423,21 +432,21 @@ func (e *Error) Error() string {
 	return fmt.Sprintf("channel %q: err %v", e.Channel, e.Err)
 }
 
-// V3Issue222TestMessage is the message expected for 'V3Issue222TestMessage' channel.
-type V3Issue222TestMessage struct {
+// TestMessageMessageFromTestingChannel is the message expected for 'TestMessageMessageFromTestingChannel' channel.
+type TestMessageMessageFromTestingChannel struct {
 	// Payload will be inserted in the message payload
 	Payload TestSchema
 }
 
-func NewV3Issue222TestMessage() V3Issue222TestMessage {
-	var msg V3Issue222TestMessage
+func NewTestMessageMessageFromTestingChannel() TestMessageMessageFromTestingChannel {
+	var msg TestMessageMessageFromTestingChannel
 
 	return msg
 }
 
-// brokerMessageToV3Issue222TestMessage will fill a new V3Issue222TestMessage with data from generic broker message
-func brokerMessageToV3Issue222TestMessage(bMsg extensions.BrokerMessage) (V3Issue222TestMessage, error) {
-	var msg V3Issue222TestMessage
+// brokerMessageToTestMessageMessageFromTestingChannel will fill a new TestMessageMessageFromTestingChannel with data from generic broker message
+func brokerMessageToTestMessageMessageFromTestingChannel(bMsg extensions.BrokerMessage) (TestMessageMessageFromTestingChannel, error) {
+	var msg TestMessageMessageFromTestingChannel
 
 	// Unmarshal payload to expected message payload format
 	err := json.Unmarshal(bMsg.Payload, &msg.Payload)
@@ -450,8 +459,8 @@ func brokerMessageToV3Issue222TestMessage(bMsg extensions.BrokerMessage) (V3Issu
 	return msg, nil
 }
 
-// toBrokerMessage will generate a generic broker message from V3Issue222TestMessage data
-func (msg V3Issue222TestMessage) toBrokerMessage() (extensions.BrokerMessage, error) {
+// toBrokerMessage will generate a generic broker message from TestMessageMessageFromTestingChannel data
+func (msg TestMessageMessageFromTestingChannel) toBrokerMessage() (extensions.BrokerMessage, error) {
 	// TODO: implement checks on message
 
 	// Marshal payload to JSON
@@ -476,11 +485,11 @@ type TestSchema struct {
 }
 
 const (
-	// V3Issue222TestPath is the constant representing the 'V3Issue222Test' channel path.
-	V3Issue222TestPath = "v3.issue222.test"
+	// TestingChannelPath is the constant representing the 'TestingChannel' channel path.
+	TestingChannelPath = "v3.issue222.test"
 )
 
 // ChannelsPaths is an array of all channels paths
 var ChannelsPaths = []string{
-	V3Issue222TestPath,
+	TestingChannelPath,
 }
