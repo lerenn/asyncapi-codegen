14e42a70f16124575277111a46255b36355cd10f
Matouš Dzivjak <matousdzivjak@gmail.com>
2024-06-27 14:43:24 +0200

    fix(templates): headers with refs (#242)
    Fixes: https://github.com/lerenn/asyncapi-codegen/issues/241

---
+++ test/v3/issues/241/generate.go
@@ -0,0 +1,5 @@
+//go:generate go run ../../../../cmd/asyncapi-codegen -p issue333 -i ./asyncapi.yaml -o ./asyncapi.gen.go
+
+package issue333
+
+// This is just to test that the generation is correct
