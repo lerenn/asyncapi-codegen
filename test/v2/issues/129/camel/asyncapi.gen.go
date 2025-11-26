2257e6f05f1524348e60d37e4ccb0d417230a32a
Gustav Sandström <gustav.sandstrom@epidemicsound.com>
2025-11-26 14:30:58 +0100

    test: regenerate issue 129 camel test files with new camelCase behavior

--- test/v2/issues/129/camel/asyncapi.gen.go
+++ test/v2/issues/129/camel/asyncapi.gen.go
@@ -242,7 +242,7 @@ func (msg V2Issue129TestMessage) toBrokerMessage() (extensions.BrokerMessage, er
 
 // TestSchema is a schema from the AsyncAPI specification required in messages
 type TestSchema struct {
-	ThisIsAProperty *string `json:"ThisIsAProperty,omitempty"`
+	ThisIsAProperty *string `json:"thisIsAProperty,omitempty"`
 }
 
 const (
